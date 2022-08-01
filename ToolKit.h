2fa8b1b2aa791ec201d5338cc178a67cc6025e0b
dengjiansen <97449427+dengjiansen@users.noreply.github.com>
2022-08-01 23:31:48 +0800

    Create ToolKit.h

---
+++ ToolKit.h
@@ -0,0 +1,12 @@
+#ifndef _TOOLKIT_H_
+#define _TOOLKIT_H_
+
+void SetCursorPosition(const int x, const int y);//设置光标位置
+
+void SetColor(int colorID);//设置文本颜色
+
+void SetBackColor();//设置文本背景色
+
+void HideCursor(bool Visible);
+#endif
+
